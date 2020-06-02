28a516e5836e69bb8e736eff00db1785100138a2
unknown <hzzmail@sjtu.org>
2020-06-02 08:44:35 +0800

    update-2020-0602-add instruction of traditional method

---
+++ egbibtex/cpbibtex.bat
@@ -0,0 +1,22 @@
+@echo off
+
+
+setlocal enabledelayedexpansion
+for  %%a in ( eg*.tex ) do (
+call makeclear
+set jobfile=%%a
+set jobname=!jobfile:~0,-4!
+echo %%a
+echo !jobfile!
+echo !jobname!
+xelatex.exe --synctex=-1 !jobfile!
+bibtex !jobname!
+xelatex.exe --synctex=-1 !jobfile!
+xelatex.exe --synctex=-1 !jobfile!
+)
+setlocal DISABLEDELAYEDEXPANSION
+
+call makeclear
+
+
+::pause
